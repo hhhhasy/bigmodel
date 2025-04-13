7fbdf7a781ca0503598af10f7cfc5a63d2207b93
asy <paul_040808@qq.com>
2025-04-13 22:13:09 +0800

    优化url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('Ollama/', include('ollamahome.urls')),
-    path('search/', include('ipsearch.urls')),
-    path('distribution/', include('ipmap.urls')),
+    path('Ollama/search/', include('ipsearch.urls')),
+    path('Ollama/distribution/', include('ipmap.urls')),
 ]
