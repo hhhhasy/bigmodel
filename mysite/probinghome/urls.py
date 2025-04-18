b67f2245b6d5bbd6fe1e55f535684f4c23af0b9c
asy <paul_040808@qq.com>
2025-04-18 09:38:03 +0800

    数据探测

--- mysite/probinghome/urls.py
+++ mysite/probinghome/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.service_list, name='service_list'),
-    path('service/<str:service_name>/', views.service_detail, name='service_detail')
+    path('service/<str:service_name>/', views.service_detail, name='service_detail'),
+    path('api/services/', views.add_service, name='add_service'),
 ]
--- mysite/probinghome/views.py
+++ mysite/probinghome/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from .models import ServiceInfo
 import os
+import json
+from django.core.exceptions import ValidationError
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return render(request, 'prob/home.html')
@@ -19,6 +22,39 @@ def service_detail(request, service_name):
         'name': service.name,
         'port': service.port,
         'auth': service.get_auth_display(),
-        'api': service.api
+        'api': service.api,
+        'banner': service.banner,
     }
     return JsonResponse(data)
+
+
+def add_service(request):
+    try:
+        data = json.loads(request.body)
+         # 打印请求体以调试
+        print("Received data:", data)
+        # 数据验证
+        if not data.get('name'):
+            raise ValidationError('服务名称不能为空')
+        if ServiceInfo.objects.filter(name=data['name']).exists():
+            raise ValidationError('服务名称已存在')
+        if not 1 <= int(data.get('port', 0)) <= 65535:
+            raise ValidationError('端口号无效')
+
+        # 创建服务
+        service = ServiceInfo.objects.create(
+            name=data['name'],
+            api=data['api'],
+            port=data['port'],
+            auth=data['auth'],
+            banner=data['banner']
+        )
+        
+        return JsonResponse({
+            'status': 'success',
+            'name': service.name
+        })
+        
+   
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': e}, status=500)
