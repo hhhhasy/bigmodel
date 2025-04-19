f52a2d6e4ce3a295fff9298627eb8a0b9138b219
Lorry <122341740+EmoLorry@users.noreply.github.com>
2025-04-19 11:02:43 +0800

    Add files via upload
    add modules about service info

--- mysite/ipsearch/models.py
+++ mysite/ipsearch/models.py
@@ -14,3 +14,17 @@ class IPInfo(models.Model):
         verbose_name = "IP信息"
         verbose_name_plural = "IP信息"
         db_table = 'ip_locations'  # 自定义表名
+
+class OllamaSearch49(models.Model):
+    ip             = models.TextField(primary_key=True)
+    status         = models.TextField()
+    protocol       = models.TextField()
+    timestamp      = models.TextField()
+    status_line    = models.TextField()
+    status_code    = models.IntegerField()
+    protocol_name  = models.TextField()
+    body_version   = models.TextField()
+
+    class Meta:
+        db_table  = 'ollama_search_4_9'
+        managed   = False    # 不由 Django 来创建/迁移
