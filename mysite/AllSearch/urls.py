bffede35d9d5d90d936eb81336c93c299a325c30
asy <paul_040808@qq.com>
2025-05-14 18:19:59 +0800

    美化界面

--- mysite/AllSearch/urls.py
+++ mysite/AllSearch/urls.py
@@ -4,10 +4,6 @@ from . import views
 urlpatterns = [
     #path('', views.index, name='index'),
     #path('search/', views.search, name='search'),
-<<<<<<< HEAD
     path('', views.search_view, name='search'),
-=======
-    path('search/', views.search_view, name='search'),
->>>>>>> 71bdc1f513e5b9e167d3bd9c25771fe92508a976
     path('api/spark/', views.call_spark_api, name='call_spark_api'),
 ]
