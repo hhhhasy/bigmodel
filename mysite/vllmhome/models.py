de5c0cc0044db5b6995464bcbf554d423c0b33ef
Betty Yang <149639632+1223BETTYYANG@users.noreply.github.com>
2025-05-18 16:26:52 +0800

    Update vllm-models.py

--- mysite/vllmhome/models.py
+++ mysite/vllmhome/models.py
@@ -1,28 +1,34 @@
 # models.py
-from django.db import models
+import re
+from django.db import connection, models
+
+def get_latest_count_md_table():
+    
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'bigmodel'")
+        tables = [row[0] for row in cursor.fetchall()]
+    pattern = re.compile(r'^vllm_(\d{8})_count_md$')
+    matched = [(m.group(1), tbl) for tbl in tables if (m := pattern.match(tbl))]
+    if not matched:
+        raise Exception("找不到符合格式的 vllma_{date}_count_md 表。")
+    latest_date, latest_table = max(matched, key=lambda x: x[0])
+    return latest_table
 
 class DailyExpose(models.Model):
-    date   = models.DateField(db_column='Date', primary_key=True)
-    counts = models.IntegerField(null=True)
+    date   = models.DateField(primary_key=True)
+    counts = models.IntegerField()
 
     class Meta:
-        db_table = 'daily_expose'
-        managed  = False
+        db_table  = 'vllm_daily_expose'
+        managed   = False
 
+def get_latest_models_counts_model():
+    table_name = get_latest_count_md_table()
+    class DynamicModelsCounts(models.Model):
+        model_name = models.CharField(max_length=255, primary_key=True)
+        count = models.IntegerField()
+        class Meta:
+                db_table = table_name
+                managed = False
 
-class ModelsCounts(models.Model):
-    model_name = models.CharField(max_length=255, primary_key=True)
-    count      = models.IntegerField()
-
-    class Meta:
-        db_table = 'models_counts'
-        managed  = False
-
-
-class DeepseekCounts(models.Model):
-    mdver = models.CharField(max_length=255, primary_key=True)
-    count = models.IntegerField()
-
-    class Meta:
-        db_table = 'deepseek_counts'
-        managed  = False
+    return DynamicModelsCounts
