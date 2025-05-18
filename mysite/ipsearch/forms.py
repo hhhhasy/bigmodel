b7dbfedc37f6503bdddf00752b3a83becf682c21
Betty Yang <149639632+1223BETTYYANG@users.noreply.github.com>
2025-05-18 17:35:06 +0800

    forms.py换掉IPInfo依赖，改用动态

--- mysite/ipsearch/forms.py
+++ mysite/ipsearch/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import IPInfo
+# from .models import IPInfo
+from .models import get_latest_table_name, create_dynamic_ip_model
 
 class SearchForm(forms.Form):
     query = forms.CharField(label='搜索', max_length=100, required=False)
